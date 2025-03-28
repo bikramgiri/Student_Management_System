a2ae95a146f21ec634ba51656331f7c48ab28e26
bikramgiri <asgbrg.github@gmail.com>
2025-03-28 07:22:57 +0545

    backend setup

---
+++ Backend/app.js
@@ -0,0 +1,12 @@
+let express = require("express")
+let app = express()
+app.set("view engine","ejs") 
+
+
+app.get("/",(req,res)=>{ // home.ejs or home
+      res.render("home")  // home.ejs file ko content lai render garnexa 
+})
+
+app.listen(3000, ()=>{
+console.log("Project has started at port 3000")
+})
