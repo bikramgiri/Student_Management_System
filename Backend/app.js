b4ef1818788b3196d8862941213aa891afccfd01
bikramgiri <asgbrg.github@gmail.com>
2025-04-01 16:30:52 +0545

    database connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,15 @@
-let express = require("express")
-let app = express()
-app.set("view engine","ejs") 
+let express = require("express") // express framework import garne
+let app = express() // express framework ko instance create garne
+
+app.set("view engine","ejs") // ejs ko view engine set garne
+require("./model/index") // importing the database connection file
 
 app.get("/",(req,res)=>{ // home.ejs or home
       res.render("home")  // home.ejs file ko content lai render garnexa 
 })
 
-app.listen(3000, ()=>{
-console.log("Project has started at port 3000")
+app.listen(3000, ()=>{ // server run on port 3000
+console.log("Project has started at port 3000") // server run on port 3000
 })
 
 
