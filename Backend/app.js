237f009e8d57dadc2800301ae3dfbe855ebdaf54
bikramgiri <asgbrg.github@gmail.com>
2025-06-23 14:43:35 +0545

    back to local host

--- Backend/app.js
+++ Backend/app.js
@@ -20,29 +20,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // **For Local Host
-// app.use(cors({ origin: process.env.FRONTEND_URL, 
-//   credentials: true // If your frontend sends cookies or auth headers
-//   }));
+app.use(cors({ origin: process.env.FRONTEND_URL, 
+  credentials: true // If your frontend sends cookies or auth headers
+  }));
 
 // **For Production
-// Define allowed origins
-const allowedOrigins = [
-  process.env.FRONTEND_URL, // For local development
-  process.env.ONLINE_URL // For production
-];
-// CORS configuration
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true // Allow cookies/auth headers if needed
-}));
+// // Define allowed origins
+// const allowedOrigins = [
+//   process.env.FRONTEND_URL, // For local development
+//   process.env.ONLINE_URL // For production
+// ];
+// // CORS configuration
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true // Allow cookies/auth headers if needed
+// }));
 
 
 // Connect to MongoDB
